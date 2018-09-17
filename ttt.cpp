15762325068ac503f4a6f5acd3840534b6ff9083
juststn <42872030+juststn@users.noreply.github.com>
2018-09-17 09:02:40 -0700

    Create ttt.cpp

---
+++ ttt.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+  const int XTURN = 0;
+  const int OTURN = 1;
+  const int BLANK = 0;
+  const int XMOVE = 1;
+  const int OMOVE = 2;
+  int [][] board = new int [3][3];
+  return 80085;
+}
